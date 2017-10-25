5cbb587727b8dce96b834dae852c1d816ee60d3d
Dan Nollette <nollette.dan@gmail.com>
2017-10-25 08:48:30 -0700

    update to-do per conditional directives lesson.

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,26 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-   <div class="container">
-      <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-      <h3>{{currentFocus}}</h3>
-      <ul>
-        <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
-      </ul>
-
-      <hr>
-      <div>
+  <div class="container">
+    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+    <h3>{{currentFocus}}</h3>
+    <ul>
+      <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
+    </ul>
+    <hr>
+    <div>
+      <div *ngIf="selectedTask">
         <h3>{{selectedTask.description}}</h3>
         <p>Task Complete? {{selectedTask.done}}</p>
+        <hr>
         <h3>Edit Task</h3>
         <label>Enter Task Description:</label>
         <input [(ngModel)]="selectedTask.description">
-        <label>Enter Task Priority (1-3):</label>
-        <br>
+        <label>Enter Task Priority (1-3):</label><br>
         <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
         <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
         <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
-     </div>
+        <button (click)="finishedEditing()">Done</button>
+      </div>
+    </div>
   </div>
   `
 })
@@ -38,17 +40,7 @@ export class AppComponent {
     new Task('Begin brainstorming possible Javascript group projects', 2),
     new Task('Add README file to the last few Angular repos on Github', 2)
   ];
-  selectedTask: Task = this.tasks[0];
-
-  priorityColor(currentTask){
-    if (currentTask.priority === 3){
-      return "bg-danger";
-    } else if (currentTask.priority ===2) {
-      return "bg-warning";
-    } else {
-      return "bg-info";
-    }
-  }
+  selectedTask = null;
 
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
@@ -61,8 +53,21 @@ export class AppComponent {
       alert("This task is not done. Better get to work!")
     }
   }
-}
 
+  priorityColor(currentTask){
+    if (currentTask.priority === 3){
+      return "bg-danger";
+    } else if (currentTask.priority ===2) {
+      return "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
+
+  finishedEditing() {
+    this.selectedTask = null;
+  }
+}
 export class Task {
   public done: boolean = false;
   constructor(public description: string, public priority: number) { }
